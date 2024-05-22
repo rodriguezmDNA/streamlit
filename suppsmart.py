b3bd0c4ed2a03ed670d7e447e2b5992111102bbe
rodriguezmDNA <joelrome88@gmail.com>
2024-05-22 13:08:53 -0700

    test exception

--- suppsmart.py
+++ suppsmart.py
@@ -15,18 +15,24 @@ stop_words.extend(extraStop)
 
 import subprocess
 
-# Run the shell command to download the spaCy model
-subprocess.run(["python", "-m", "spacy", "download", "en_core_web_sm"], check=True)
+# Function to download the spaCy model if not already present
+def download_spacy_model():
+    try:
+        subprocess.run(["python", "-m", "spacy", "download", "en_core_web_sm"], check=True)
+    except subprocess.CalledProcessError as e:
+        st.error(f"Failed to download spaCy model: {e}")
+        return False
+    return True
 
-
+# Check if spaCy model is already downloaded
 try:
-    import spacy
     nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
-except Exception as e:
-    st.write(f"Error loading spaCy model: {e}")
-    
-import spacy #Language model
-nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner']) 
+except OSError:
+    if download_spacy_model():
+        nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
+    else:
+        st.stop()  # Stop the script execution if the model download fails
+
 ############################################################
 
 import streamlit as st
